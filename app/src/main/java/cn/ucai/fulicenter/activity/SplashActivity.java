649c4983260249f172fb146198ae604a4470c237
chenjingren <1182508339@qq.com>
2016-10-14 15:09:14 +0800

    闪屏跳转到MainActivity

--- app/src/main/java/cn/ucai/fulicenter/activity/SplashActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/SplashActivity.java
@@ -1,15 +1,38 @@
 package cn.ucai.fulicenter.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import cn.ucai.fulicenter.R;
 
 public class SplashActivity extends Activity {
 
+    private long sleepTime = 2000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                long startTime = System.currentTimeMillis();
+                long costTime = System.currentTimeMillis() - startTime;
+                if (sleepTime-costTime>0){
+                    try {
+                        Thread.sleep(sleepTime-costTime);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+            }
+        }).start();
     }
 }
