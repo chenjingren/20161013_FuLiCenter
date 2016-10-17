4a8fc25853d6a1ff03df4e67f40d62c9082e4e70
chenjingren <1182508339@qq.com>
2016-10-17 15:42:12 +0800

    添加FooterViewHolder

--- app/src/main/java/cn/ucai/fulicenter/holder/FooterViewHolder.java
+++ app/src/main/java/cn/ucai/fulicenter/holder/FooterViewHolder.java
@@ -1,7 +1,18 @@
 package cn.ucai.fulicenter.holder;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import cn.ucai.fulicenter.R;
+
 /**
  * Created by ACherish on 2016/10/17.
  */
-public class FooterViewHolder {
+public class FooterViewHolder extends RecyclerView.ViewHolder{
+    TextView mtvFooter;
+    public FooterViewHolder(View itemView) {
+        super(itemView);
+        mtvFooter = (TextView) itemView.findViewById(R.id.tvFooter);
+    }
 }
