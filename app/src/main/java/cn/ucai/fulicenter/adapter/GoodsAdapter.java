a4dc2b64cc3e569c5808ad2c33b827e7fd36aa53
chenjingren <1182508339@qq.com>
2016-10-17 11:24:16 +0800

    add GoodsAdapter

---
+++ app/src/main/java/cn/ucai/fulicenter/adapter/GoodsAdapter.java
@@ -0,0 +1,24 @@
+package cn.ucai.fulicenter.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by ACherish on 2016/10/17.
+ */
+public class GoodsAdapter extends RecyclerView.Adapter{
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
