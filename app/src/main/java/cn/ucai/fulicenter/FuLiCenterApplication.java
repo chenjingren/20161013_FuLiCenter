f63d451388aac5eed18f30f66284742e2580bb10
chenjingren <1182508339@qq.com>
2016-10-17 20:49:34 +0800

    mod application

--- app/src/main/java/cn/ucai/fulicenter/FuLiCenterApplication.java
+++ app/src/main/java/cn/ucai/fulicenter/FuLiCenterApplication.java
@@ -7,17 +7,21 @@ import android.content.Context;
  * Created by ACherish on 2016/10/17.
  */
 public class FuLiCenterApplication extends Application{
-    public static Context applicationContext;
+    //public static Context applicationContext;
+    public static FuLiCenterApplication application;
     private static FuLiCenterApplication instance;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        applicationContext = this;
+        application = this;
         instance = this;
     }
 
     public static FuLiCenterApplication getInstance(){
+        if (instance==null){
+            instance = new FuLiCenterApplication();
+        }
         return instance;
     }
 
