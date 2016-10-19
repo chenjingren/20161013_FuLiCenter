15a611ca3845ea06bf82915dd70d556ee71e9ae6
chenjingren <1182508339@qq.com>
2016-10-19 11:44:27 +0800

    add downloadBoutique( )

--- app/src/main/java/cn/ucai/fulicenter/net/NetDao.java
+++ app/src/main/java/cn/ucai/fulicenter/net/NetDao.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.media.MediaPlayer;
 
 import cn.ucai.fulicenter.I;
+import cn.ucai.fulicenter.bean.BoutiqueBean;
 import cn.ucai.fulicenter.bean.GoodsDetailsBean;
 import cn.ucai.fulicenter.bean.NewGoodsBean;
 import cn.ucai.fulicenter.utils.OkHttpUtils;
@@ -28,6 +29,13 @@ public class NetDao {
         utils.setRequestUrl(I.REQUEST_FIND_GOOD_DETAILS)
                 .addParam(I.GoodsDetails.KEY_GOODS_ID,String.valueOf(goodsId))
                 .targetClass(GoodsDetailsBean.class)
+                .execute(listener);
+    }
+
+    public static void downloadBoutique(Context context, OkHttpUtils.OnCompleteListener<BoutiqueBean[]> listener){
+        OkHttpUtils utils = new OkHttpUtils(context);
+        utils.setRequestUrl(I.REQUEST_FIND_BOUTIQUES)
+                .targetClass(BoutiqueBean[].class)
                 .execute(listener);
     }
 }
