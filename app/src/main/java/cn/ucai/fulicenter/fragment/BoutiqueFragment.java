2e9ddf14b23cb74e583058dea6ed07d881782d6d
chenjingren <1182508339@qq.com>
2016-10-19 11:20:30 +0800

    add BoutiqueFragment

---
+++ app/src/main/java/cn/ucai/fulicenter/fragment/BoutiqueFragment.java
@@ -0,0 +1,29 @@
+package cn.ucai.fulicenter.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import cn.ucai.fulicenter.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BoutiqueFragment extends Fragment {
+
+    public BoutiqueFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View layout = inflater.inflate(R.layout.fragment_boutique, container, false);
+        
+        return layout;
+    }
+
+}
