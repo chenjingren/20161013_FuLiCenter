578966329353e1c3f575c3e0bebd30ec53858e4e
chenjingren <1182508339@qq.com>
2016-10-19 16:12:00 +0800

    添加BaseFragment

---
+++ app/src/main/java/cn/ucai/fulicenter/fragment/BaseFragment.java
@@ -0,0 +1,25 @@
+package cn.ucai.fulicenter.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ACherish on 2016/10/19.
+ */
+public abstract class BaseFragment extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        initView();
+        initData();
+        setListener();
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+    
+    protected abstract void initView();
+    protected abstract void initData();
+    protected abstract void setListener();
+}
