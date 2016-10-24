798432f8b900850d7f7de9f33ae7004abff1c9c9
chenjingren <1182508339@qq.com>
2016-10-24 20:55:07 +0800

    添加数据库的工具类

---
+++ app/src/main/java/cn/ucai/fulicenter/dao/DBOpenHelper.java
@@ -0,0 +1,7 @@
+package cn.ucai.fulicenter.dao;
+
+/**
+ * Created by ACherish on 2016/10/24.
+ */
+public class DBOpenHelper {
+}
---
+++ app/src/main/java/cn/ucai/fulicenter/utils/SharePreferencesUtils.java
@@ -0,0 +1,7 @@
+package cn.ucai.fulicenter.utils;
+
+/**
+ * Created by ACherish on 2016/10/24.
+ */
+public class SharePreferencesUtils {
+}
